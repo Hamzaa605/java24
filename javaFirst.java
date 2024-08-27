065bde568d8a699e81baccbb737a632a55fe766e
Hamzaa605 <hs073545@gmail.com>
2024-08-27 09:03:18 +0530

    1 commit

---
+++ javaFirst.java
@@ -0,0 +1,12 @@
+class javaFirst
+{
+	public static void main(String args[])
+	{
+		System.out.print("I am learning java"); 
+	}
+}
+// to comment
+/*
+--> javac filename.javaFirst
+--> java classname
+*/
