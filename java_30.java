ded7709644d6d3b3a86fbc78b785e3222d171560
Hamzaa605 <hs073545@gmail.com>
2024-08-30 08:58:04 +0530

    2 commit

---
+++ java_30.java
@@ -0,0 +1,23 @@
+class java_30
+{
+	public static void main(String args[])
+	{
+		
+		int a=3;
+		int b=5;
+		
+		System.out.println(a==b);
+		
+		
+		int e=3;
+		int w=5;
+		
+		System.out.println(a!=b);
+		
+		int x=3;
+		int y=5;
+		
+		System.out.println(a<=b);
+		
+	}	
+}
---
+++ java_t.java
@@ -0,0 +1,10 @@
+//ternary opreater
+class java_t
+{
+	public static void main(String args[])
+	{
+		System.out.println(true?"hello":"bye");
+		
+		System.out.println(false?"hello":"bye");
+	}
+}
