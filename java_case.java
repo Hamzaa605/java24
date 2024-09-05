af72faa0fe2460d4cb0c2b31f7fd80694c29086a
Hamzaa605 <hs073545@gmail.com>
2024-09-05 08:57:26 +0530

    4 commit

---
+++ java_case.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class java_case
+{
+	public static void main(String args[])
+	{
+		switch()
+		{
+		case 1:
+		System.out.println("case 1");
+		break;
+		
+		case 2:
+		System.out.println("case 2");
+		break;
+		
+		case 3:
+		System.out.println("case 3");
+		break;
+		
+	   default:
+	   System.out.println("No matching case");
+		}
+	}
+}
