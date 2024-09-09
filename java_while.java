ca29a6295b253bbe0c7f54b66ef458637f0e16a5
Hamzaa605 <hs073545@gmail.com>
2024-09-09 08:53:55 +0530

    5 commit

---
+++ java_while.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class java_while{
+	 
+	 public static void main(String args[])
+	 {
+		 int i=10;
+		 while(i<=58)
+		 {
+			System.out.println(i);
+			i++;
+		 }
+		 
+	 }
+	
+}
---
+++ java_while3.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class java_while3
+{
+	 
+	 public static void main(String args[])
+	 {
+		Scanner sc=new Scanner(System.in);
+		int n=0;
+		int count=0;
+		
+			while(n>=0)
+			{
+				n=sc.nextInt();
+				count++;
+			}
+			System.out.println("Count"+(count-1));
+	}
+}
