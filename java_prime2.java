86f22af9cb6f265c84ead31256f678d310fb3e6a
truptig-2512 <truptigorivale00@gmail.com>
2024-09-12 08:52:31 +0530

    7 commit

---
+++ java_prime2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class java_prime2
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int primecount=0;
+		for(int n=1;n<=100;n++)
+		{
+			int count=0;
+		for(int i=1;i<=n;i++)
+		{
+			if (n%i==0)
+			{
+				
+				count++;
+			}
+		}
+		
+		
+		if(count==2)
+		{
+			System.out.println(n);
+			primecount++;
+		}
+		
+		}
+	System.out.println("count:"+primecount);	
+	}
+}
---
+++ java_table2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class java_table2
+{
+	public static void main(String args[])
+	{
+		
+		for(int j=1;j<=10;j++)
+		{
+			int n=7;
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(n*i);
+		}
+		System.out.println("=============");
+		}
+	}
+}
